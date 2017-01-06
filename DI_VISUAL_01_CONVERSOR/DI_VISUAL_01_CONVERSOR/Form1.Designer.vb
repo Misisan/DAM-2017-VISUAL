af4e8f6bb7a587d9096c21935e41ccb979715e63
Miguel A. Simeón <miguelsimeon@gmail.com>
2017-01-06 21:08:21 +0100

    DI - Visual Studio - ACT 01 - Conversor decimal-binario-hexadecimal

--- DI_VISUAL_01_CONVERSOR/DI_VISUAL_01_CONVERSOR/Form1.Designer.vb
+++ DI_VISUAL_01_CONVERSOR/DI_VISUAL_01_CONVERSOR/Form1.Designer.vb
@@ -22,9 +22,201 @@ Partial Class Form1
     'No lo modifique con el editor de código.
     <System.Diagnostics.DebuggerStepThrough()> _
     Private Sub InitializeComponent()
-        components = New System.ComponentModel.Container()
+        Me.Label1 = New System.Windows.Forms.Label()
+        Me.txt_origen = New System.Windows.Forms.TextBox()
+        Me.Label2 = New System.Windows.Forms.Label()
+        Me.txt_destino = New System.Windows.Forms.TextBox()
+        Me.ori_dec = New System.Windows.Forms.RadioButton()
+        Me.ori_bin = New System.Windows.Forms.RadioButton()
+        Me.ori_hexa = New System.Windows.Forms.RadioButton()
+        Me.des_hexa = New System.Windows.Forms.RadioButton()
+        Me.des_bin = New System.Windows.Forms.RadioButton()
+        Me.des_dec = New System.Windows.Forms.RadioButton()
+        Me.group_ori = New System.Windows.Forms.GroupBox()
+        Me.group_dest = New System.Windows.Forms.GroupBox()
+        Me.btn_calc = New System.Windows.Forms.Button()
+        Me.btn_limpiar = New System.Windows.Forms.Button()
+        Me.group_ori.SuspendLayout()
+        Me.group_dest.SuspendLayout()
+        Me.SuspendLayout()
+        '
+        'Label1
+        '
+        Me.Label1.AutoSize = True
+        Me.Label1.Location = New System.Drawing.Point(35, 38)
+        Me.Label1.Name = "Label1"
+        Me.Label1.Size = New System.Drawing.Size(38, 13)
+        Me.Label1.TabIndex = 0
+        Me.Label1.Text = "Origen"
+        '
+        'txt_origen
+        '
+        Me.txt_origen.Location = New System.Drawing.Point(79, 35)
+        Me.txt_origen.Name = "txt_origen"
+        Me.txt_origen.Size = New System.Drawing.Size(100, 20)
+        Me.txt_origen.TabIndex = 1
+        '
+        'Label2
+        '
+        Me.Label2.AutoSize = True
+        Me.Label2.Location = New System.Drawing.Point(210, 38)
+        Me.Label2.Name = "Label2"
+        Me.Label2.Size = New System.Drawing.Size(43, 13)
+        Me.Label2.TabIndex = 2
+        Me.Label2.Text = "Destino"
+        '
+        'txt_destino
+        '
+        Me.txt_destino.Location = New System.Drawing.Point(254, 35)
+        Me.txt_destino.Name = "txt_destino"
+        Me.txt_destino.ReadOnly = True
+        Me.txt_destino.Size = New System.Drawing.Size(100, 20)
+        Me.txt_destino.TabIndex = 3
+        '
+        'ori_dec
+        '
+        Me.ori_dec.AutoSize = True
+        Me.ori_dec.Location = New System.Drawing.Point(19, 24)
+        Me.ori_dec.Name = "ori_dec"
+        Me.ori_dec.Size = New System.Drawing.Size(63, 17)
+        Me.ori_dec.TabIndex = 4
+        Me.ori_dec.TabStop = True
+        Me.ori_dec.Text = "Decimal"
+        Me.ori_dec.UseVisualStyleBackColor = True
+        '
+        'ori_bin
+        '
+        Me.ori_bin.AutoSize = True
+        Me.ori_bin.Location = New System.Drawing.Point(19, 47)
+        Me.ori_bin.Name = "ori_bin"
+        Me.ori_bin.Size = New System.Drawing.Size(57, 17)
+        Me.ori_bin.TabIndex = 5
+        Me.ori_bin.TabStop = True
+        Me.ori_bin.Text = "Binario"
+        Me.ori_bin.UseVisualStyleBackColor = True
+        '
+        'ori_hexa
+        '
+        Me.ori_hexa.AutoSize = True
+        Me.ori_hexa.Location = New System.Drawing.Point(19, 70)
+        Me.ori_hexa.Name = "ori_hexa"
+        Me.ori_hexa.Size = New System.Drawing.Size(86, 17)
+        Me.ori_hexa.TabIndex = 6
+        Me.ori_hexa.TabStop = True
+        Me.ori_hexa.Text = "Hexadecimal"
+        Me.ori_hexa.UseVisualStyleBackColor = True
+        '
+        'des_hexa
+        '
+        Me.des_hexa.AutoSize = True
+        Me.des_hexa.Location = New System.Drawing.Point(21, 68)
+        Me.des_hexa.Name = "des_hexa"
+        Me.des_hexa.Size = New System.Drawing.Size(86, 17)
+        Me.des_hexa.TabIndex = 9
+        Me.des_hexa.TabStop = True
+        Me.des_hexa.Text = "Hexadecimal"
+        Me.des_hexa.UseVisualStyleBackColor = True
+        '
+        'des_bin
+        '
+        Me.des_bin.AutoSize = True
+        Me.des_bin.Location = New System.Drawing.Point(21, 45)
+        Me.des_bin.Name = "des_bin"
+        Me.des_bin.Size = New System.Drawing.Size(57, 17)
+        Me.des_bin.TabIndex = 8
+        Me.des_bin.TabStop = True
+        Me.des_bin.Text = "Binario"
+        Me.des_bin.UseVisualStyleBackColor = True
+        '
+        'des_dec
+        '
+        Me.des_dec.AutoSize = True
+        Me.des_dec.Location = New System.Drawing.Point(21, 22)
+        Me.des_dec.Name = "des_dec"
+        Me.des_dec.Size = New System.Drawing.Size(63, 17)
+        Me.des_dec.TabIndex = 7
+        Me.des_dec.TabStop = True
+        Me.des_dec.Text = "Decimal"
+        Me.des_dec.UseVisualStyleBackColor = True
+        '
+        'group_ori
+        '
+        Me.group_ori.Controls.Add(Me.ori_hexa)
+        Me.group_ori.Controls.Add(Me.ori_dec)
+        Me.group_ori.Controls.Add(Me.ori_bin)
+        Me.group_ori.Location = New System.Drawing.Point(38, 85)
+        Me.group_ori.Name = "group_ori"
+        Me.group_ori.Size = New System.Drawing.Size(141, 98)
+        Me.group_ori.TabIndex = 10
+        Me.group_ori.TabStop = False
+        Me.group_ori.Text = "Sistema Origen"
+        '
+        'group_dest
+        '
+        Me.group_dest.Controls.Add(Me.des_bin)
+        Me.group_dest.Controls.Add(Me.des_dec)
+        Me.group_dest.Controls.Add(Me.des_hexa)
+        Me.group_dest.Location = New System.Drawing.Point(214, 87)
+        Me.group_dest.Name = "group_dest"
+        Me.group_dest.Size = New System.Drawing.Size(140, 96)
+        Me.group_dest.TabIndex = 11
+        Me.group_dest.TabStop = False
+        Me.group_dest.Text = "Sistema Destino"
+        '
+        'btn_calc
+        '
+        Me.btn_calc.Location = New System.Drawing.Point(38, 200)
+        Me.btn_calc.Name = "btn_calc"
+        Me.btn_calc.Size = New System.Drawing.Size(315, 30)
+        Me.btn_calc.TabIndex = 12
+        Me.btn_calc.Text = "CALCULAR"
+        Me.btn_calc.UseVisualStyleBackColor = True
+        '
+        'btn_limpiar
+        '
+        Me.btn_limpiar.Location = New System.Drawing.Point(166, 236)
+        Me.btn_limpiar.Name = "btn_limpiar"
+        Me.btn_limpiar.Size = New System.Drawing.Size(63, 24)
+        Me.btn_limpiar.TabIndex = 13
+        Me.btn_limpiar.Text = "limpiar"
+        Me.btn_limpiar.UseVisualStyleBackColor = True
+        '
+        'Form1
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
         Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(398, 272)
+        Me.Controls.Add(Me.btn_limpiar)
+        Me.Controls.Add(Me.btn_calc)
+        Me.Controls.Add(Me.group_dest)
+        Me.Controls.Add(Me.group_ori)
+        Me.Controls.Add(Me.txt_destino)
+        Me.Controls.Add(Me.Label2)
+        Me.Controls.Add(Me.txt_origen)
+        Me.Controls.Add(Me.Label1)
+        Me.Name = "Form1"
         Me.Text = "Form1"
+        Me.group_ori.ResumeLayout(False)
+        Me.group_ori.PerformLayout()
+        Me.group_dest.ResumeLayout(False)
+        Me.group_dest.PerformLayout()
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
     End Sub
 
+    Friend WithEvents Label1 As Label
+    Friend WithEvents txt_origen As TextBox
+    Friend WithEvents Label2 As Label
+    Friend WithEvents ori_dec As RadioButton
+    Friend WithEvents ori_bin As RadioButton
+    Friend WithEvents ori_hexa As RadioButton
+    Friend WithEvents des_hexa As RadioButton
+    Friend WithEvents des_bin As RadioButton
+    Friend WithEvents des_dec As RadioButton
+    Friend WithEvents group_ori As GroupBox
+    Friend WithEvents group_dest As GroupBox
+    Friend WithEvents btn_calc As Button
+    Friend WithEvents btn_limpiar As Button
+    Public WithEvents txt_destino As TextBox
 End Class
--- DI_VISUAL_01_CONVERSOR/DI_VISUAL_01_CONVERSOR/Form1.vb
+++ DI_VISUAL_01_CONVERSOR/DI_VISUAL_01_CONVERSOR/Form1.vb
@@ -1,3 +1,138 @@
 ﻿Public Class Form1
+    Private Sub btn_limpiar_Click(sender As Object, e As EventArgs) Handles btn_limpiar.Click
+        ori_bin.Checked = False
+        ori_dec.Checked = False
+        ori_hexa.Checked = False
+        des_bin.Checked = False
+        des_dec.Checked = False
+        des_hexa.Checked = False
+        txt_destino.Text = ""
+        txt_origen.Text = ""
+    End Sub
+
+    Private Sub txt_origen_TextChanged(sender As Object, e As EventArgs) Handles txt_origen.TextChanged
+
+        If Not IsNumeric(txt_origen.Text) And ori_hexa.Checked = False Then
+
+        End If
+    End Sub
+
+    Private Sub btn_calc_Click(sender As Object, e As EventArgs) Handles btn_calc.Click
+
+        'Decimal
+        If ori_dec.Checked Then
+            If IsNumeric(txt_origen.Text) Then
+
+
+                If des_dec.Checked Then         'Decimal -> Decimal
+                    txt_destino.Text = txt_origen.Text
+
+
+                ElseIf des_bin.Checked Then     'Decimal -> Binario
+                    Dim resto As String
+                    Dim num As Long = Val(txt_origen.Text)
+                    Dim res As String = ""
+
+                    Do
+                        resto = num Mod 2
+                        res = res & Trim(Str(resto))
+                        num = Int(num / 2)
+                    Loop Until num < 2
+
+                    If (num = 1) Then
+                        res = "1" & StrReverse(res)
+                    Else
+                        res = StrReverse(res)
+                    End If
+
+                    txt_destino.Text = res
+
+                ElseIf des_hexa.Checked Then        'Decimal -> Hexadecimal
+                    txt_destino.Text = Hex(CInt(txt_origen.Text))
+                Else
+                    MsgBox("Debes seleccionar alguna opción de conversión en destino")
+                End If
+            Else
+                MsgBox("Debes introducir únicamente dígitos")
+                txt_origen.Text = ""
+            End If
+
+            'Binario
+        ElseIf ori_bin.Checked Then
+            If IsNumeric(txt_origen.Text) Then
+                If des_dec.Checked Then             'Binario -> Decimal
+
+                    Dim binario As String = StrReverse(txt_origen.Text)
+                    Dim res As Integer = 0
+                    Dim multiplicador As Integer = 1
+                    Dim caracter As String
+
+                    For i = 1 To Len(binario)
+                        caracter = Mid(binario, i, 1)
+                        res = res + (multiplicador * CInt(caracter))
+                        multiplicador = multiplicador * 2
+                    Next i
+
+                    txt_destino.Text = CStr(res)
+
+                ElseIf des_bin.Checked Then         'Binario -> Binario
+                    txt_destino.Text = txt_origen.Text
+
+                ElseIf des_hexa.Checked Then        'Binario -> Hexadecimal
+                    Dim binario As String = StrReverse(txt_origen.Text)
+                    Dim res As Integer = 0
+                    Dim multiplicador As Integer = 1
+                    Dim caracter As String
+
+                    For i = 1 To Len(binario)
+                        caracter = Mid(binario, i, 1)
+                        res = res + (multiplicador * CInt(caracter))
+                        multiplicador = multiplicador * 2
+                    Next i
+
+                    txt_destino.Text = Hex(CStr(res))
+                Else
+                    MsgBox("Debes seleccionar alguna opción de conversión en destino")
+                End If
+            Else
+                MsgBox("Debes introducir únicamente 0 y 1")
+                txt_origen.Text = ""
+            End If
+
+            'Hexadecimal
+        ElseIf ori_hexa.Checked Then
+
+            If des_dec.Checked Then             'Hexadecimal -> Decimal
+                txt_destino.Text = CStr(Convert.ToInt32(txt_origen.Text, 16))
+
+            ElseIf des_bin.Checked Then         'Hexadecimal -> Binario
+                Dim resto As String
+                Dim num As Long = Val(Convert.ToInt32(txt_origen.Text, 16))
+                Dim res As String = ""
+
+                Do
+                    resto = num Mod 2
+                    res = res & Trim(Str(resto))
+                    num = Int(num / 2)
+                Loop Until num < 2
+
+                If (num = 1) Then
+                    res = "1" & StrReverse(res)
+                Else
+                    res = StrReverse(res)
+                End If
+
+                txt_destino.Text = res
+
+            ElseIf des_hexa.Checked Then        'Hexadecimal -> Hexadecimal
+                txt_destino.Text = txt_origen.Text
+            Else
+                MsgBox("Debes seleccionar alguna opción de conversión en destino")
+            End If
+        Else
+            MsgBox("Debes seleccionar alguna opción de conversión en origen")
+        End If
+
+    End Sub
 
 End Class
